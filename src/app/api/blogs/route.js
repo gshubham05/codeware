da5b65aca5fc2fbddbeacd98f3b68a7acea5c434
Shubham Goyal <111879491+gshubham05@users.noreply.github.com>
2025-07-18 15:09:04 +0530

    Update route.js

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,7 +1,7 @@
 // app/api/blogs/route.js
 import { NextResponse } from "next/server";
 import Blog from "../../models/Blog";
-import { connectDB } from "../lib/db";
+import { connectDB } from "../../lib/db";
 
 // GET - fetch all blogs
 export async function GET() {
